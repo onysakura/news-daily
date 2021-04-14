2316da6c10d721af2797992351c520a5de5c1194
guojp <gjp0609@@foxmail.com>
2021-04-14 10:00:37 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const issue = require('./src/issue');
+
 const run = async (date) => {
     console.log(date);
+    const res = await issue.open({
+        owner: 'guojp-temp',
+        repo: 'v2ex-rss',
+        title: `v2ex rss @${new Date(date).toISOString().slice(0, 10)}`,
+        body: 'asdd'
+    });
+    const issueNumber = res.data.number;
+    await issue.lock({
+        owner: 'guojp-temp',
+        repo: 'v2ex-rss',
+        issueNumber,
+    });
 }
 
 run(new Date()).catch(err => {
